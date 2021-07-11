4ada955b39666de87eb264dfd1ed0d69d5157036
Michał Leśniewski <mlesniew@gmail.com>
2021-07-11 18:58:00 +0200

    Reformat code

--- elicznik.py
+++ elicznik.py
@@ -15,14 +15,10 @@ parser.add_argument("meter_id")
 
 args = parser.parse_args()
 
-payload = {
-                'username': args.username,
-                'password': args.password,
-                'service': 'https://elicznik.tauron-dystrybucja.pl'
-}
+payload = {"username": args.username, "password": args.password, "service": "https://elicznik.tauron-dystrybucja.pl"}
 
-LOGIN_URL = 'https://logowanie.tauron-dystrybucja.pl/login'
-CHART_URL = 'https://elicznik.tauron-dystrybucja.pl/index/charts'
+LOGIN_URL = "https://logowanie.tauron-dystrybucja.pl/login"
+CHART_URL = "https://elicznik.tauron-dystrybucja.pl/index/charts"
 
 
 # Workaround for https://github.com/psf/requests/issues/4775
@@ -30,33 +26,31 @@ class TLSAdapter(requests.adapters.HTTPAdapter):
     def init_poolmanager(self, connections, maxsize, block=False):
         """Create and initialize the urllib3 PoolManager."""
         ctx = ssl.create_default_context()
-        ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+        ctx.set_ciphers("DEFAULT@SECLEVEL=1")
         self.poolmanager = poolmanager.PoolManager(
-                num_pools=connections,
-                maxsize=maxsize,
-                block=block,
-                ssl_version=ssl.PROTOCOL_TLS,
-                ssl_context=ctx)
+            num_pools=connections, maxsize=maxsize, block=block, ssl_version=ssl.PROTOCOL_TLS, ssl_context=ctx
+        )
+
 
 session = requests.session()
-session.mount('https://', TLSAdapter())
+session.mount("https://", TLSAdapter())
 
 p = session.get(LOGIN_URL)
 p = session.post(LOGIN_URL, data=payload)
 
 
 chart = {
-        #change timedelta to get data from another days (1 for yesterday)
-        "dane[chartDay]": (datetime.datetime.now() - datetime.timedelta(1)).strftime('%d.%m.%Y'),
-        "dane[paramType]": "day",
-        "dane[smartNr]": args.meter_id,
-        #comment if don't want generated energy data in JSON output:
-        "dane[checkOZE]": "on"
-        }
+    # change timedelta to get data from another days (1 for yesterday)
+    "dane[chartDay]": (datetime.datetime.now() - datetime.timedelta(1)).strftime("%d.%m.%Y"),
+    "dane[paramType]": "day",
+    "dane[smartNr]": args.meter_id,
+    # comment if don't want generated energy data in JSON output:
+    "dane[checkOZE]": "on",
+}
 
 r = session.post(CHART_URL, data=chart)
 print(r.text)
 
-#Optionally write JSON to file
-#with open('file.json', 'wb') as f:
+# Optionally write JSON to file
+# with open('file.json', 'wb') as f:
 #    f.write(r.content)
