ebc04472baaeb1170c6ffeb352d2e741b8aba474
Paweł T. Jochym <pawel.jochym@ifj.edu.pl>
2023-01-11 09:16:32 +0100

    First attempt to adapt to changed API

--- src/elicznik/elicznik.py
+++ src/elicznik/elicznik.py
@@ -3,11 +3,15 @@
 import datetime
 
 from .session import Session
-
+from collections import defaultdict
 
 class ELicznik:
     LOGIN_URL = "https://logowanie.tauron-dystrybucja.pl/login"
     CHART_URL = "https://elicznik.tauron-dystrybucja.pl/index/charts"
+    URL_ENERGY = "https://elicznik.tauron-dystrybucja.pl/energia/api"
+    URL_READINGS = "https://elicznik.tauron-dystrybucja.pl/odczyty/api"
+    CHART_URL = URL_ENERGY
+    DATA_URL = "https://elicznik.tauron-dystrybucja.pl/energia/do/dane"
 
     def __init__(self, username, password):
         self.username = username
@@ -39,11 +43,15 @@ class ELicznik:
             data={
                 # "dane[smartNr]": "?"
                 # "dane[chartDay]": date.strftime("%d.%m.%Y"),
-                "dane[paramType]": "csv",
-                "dane[trybCSV]": "godzin",
-                "dane[startDay]": start_date.strftime("%d.%m.%Y"),
-                "dane[endDay]": end_date.strftime("%d.%m.%Y"),
-                "dane[checkOZE]": "on",
+                # "dane[paramType]": "csv",
+                # "dane[trybCSV]": "godzin",
+                # "dane[startDay]": start_date.strftime("%d.%m.%Y"),
+                # "dane[endDay]": end_date.strftime("%d.%m.%Y"),
+                # "dane[checkOZE]": "on",
+                "from": start_date.strftime("%d.%m.%Y"),
+                "to" : end_date.strftime("%d.%m.%Y"),
+                "profile": "full time",
+                "type": "consum",
             },
         ).json()
 
@@ -67,3 +75,45 @@ class ELicznik:
             (timestamp, float(consumed.get(timestamp)), float(produced.get(timestamp)))
             for timestamp in set(consumed) | set(produced)
         )
+
+    def get_raw_data(self, start_date, end_date=None):
+        end_date = end_date or start_date
+        return self.session.post(
+            self.DATA_URL,
+            data={
+                "form[from]": start_date.strftime("%d.%m.%Y"),
+                "form[to]" : end_date.strftime("%d.%m.%Y"),
+                "form[type]": "godzin", # or "dzien"
+                "form[consum]": 1,
+                "form[oze]": 1,
+                "form[fileType]": "CSV", # or "XLS"
+            },
+        ).content.decode()
+
+    def get_data(self, start_date, end_date=None):
+        raw_data = self.get_raw_data(start_date, end_date).split('\n')[1:]
+        cons = defaultdict(float)
+        prod = defaultdict(float)
+        for l in raw_data:
+            print(l)
+            try:
+                t, v, r = l.split(';')[:3]
+            except ValueError:
+                continue
+            date, hour = t.split()
+            h, m = hour.split(':')
+            timestamp = datetime.datetime.strptime(date, "%d.%m.%Y")
+            timestamp += datetime.timedelta(hours=int(h), minutes=int(m))
+            if r=='pobór':
+                cons[timestamp] = v
+            elif r=='oddanie':
+                prod[timestamp] = v
+                pass
+        # TODO
+        # This probably drops the data from the double hour during DST change
+        # Needs to be investigated and fixed
+        return sorted(
+            (timestamp, float(cons[timestamp]), float(prod[timestamp]))
+            for timestamp in set(cons) | set(prod)
+        )
+
---
+++ test_el.py
@@ -0,0 +1,34 @@
+import datetime
+import configparser
+import elicznik
+
+# read config file
+config = configparser.ConfigParser()
+config.read(['config.ini','config-local.ini'])
+
+username = config['ACCOUNT']['USERNAME']
+password = config['ACCOUNT']['PASSWORD']
+meter_id = config['ACCOUNT']['METER_ID']
+
+with elicznik.ELicznik(username, password) as m:
+    # date range
+    print("July 2021")
+
+    readings = m.get_data(datetime.date(2022, 7, 1), datetime.date(2022, 7, 2))
+
+    for timestamp, consumed, produced in readings:
+        print(timestamp, consumed, produced)
+
+    # for l in readings.split('\n'):
+    #     # timestamp, consumed, produced = split
+    #     # print(timestamp, consumed, produced)
+    #     print(l.split(';'))
+
+    # single day
+    print("Yesterday")
+
+    yesterday = datetime.date.today() - datetime.timedelta(days=1)
+    readings = m.get_data(yesterday)
+
+    for timestamp, consumed, produced in readings:
+        print(timestamp, consumed, produced)
