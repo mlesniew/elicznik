643f1e63f90c48ca7f090a89e3abd6d840ab270c
Michał Leśniewski <mlesniew@gmail.com>
2023-04-07 21:30:09 +0200

    Update Python classifiers

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='elicznik',
-    version='1.4.2',
+    version='1.4.3',
     description='Tauron eLicznik scrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -15,13 +15,15 @@ setup(
     author='Michał Leśniewski',
     author_email='mlesniew@gmail.com',
     classifiers=[
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
         'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3',
+        'Topic :: Home Automation',
+        'Topic :: Internet',
+        'Topic :: Utilities',
     ],
     keywords='elicznik, tauron, scrapper',
     package_dir={'': 'src'},
