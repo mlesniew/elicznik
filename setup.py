691d56aee03e5cae2ad1791d107dd48fea7c49ec
Michał Leśniewski <mlesniew@gmail.com>
2024-05-19 18:24:54 +0200

    Bump version to 2.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='elicznik',
-    version='2.2.0',
+    version='2.2.1',
     description='Tauron eLicznik scrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
